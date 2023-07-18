0ab9818e03a293bae45372f0b806f138818d70f3
hemanth <applicationcomputer257@gmail.com>
2023-07-18 16:51:50 +0530

    get request

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
-
+const fs=require("fs")
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'hello from the server side',
-    app: 'Naturos',
-  });
-});
+const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.post('/', (req, res) => {
-  res.send('You can post to the this end point');
-});
+app.get('/api/v1/tours',(req,res)=>{
+res.status(200).json({
+  status:"success",
+   results:tours.length,
+  data:{tours}
+})
+})
 
 app.listen(port, () => {
   console.log(`server started listening at port number ${port} `);
